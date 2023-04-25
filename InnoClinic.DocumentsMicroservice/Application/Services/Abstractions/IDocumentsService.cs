064ae4fdce93ba400082394b4519789d2126e388
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-25 13:25:06 +0300

    add services

---
+++ InnoClinic.DocumentsMicroservice/Application/Services/Abstractions/IDocumentsService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOs.Incoming;
+using Application.DTOs.Outgoing;
+
+namespace Application.Services.Abstractions;
+
+public interface IDocumentsService
+{
+    Task<string> UploadAsync(DocumentIncomingDto incomingDto, string partitionName);
+    Task<DocumentOutgoingDto> DownloadAsync(string documentName, string partitionName);
+    Task DeleteAsync(string documentName, string partitionName);
+}
---
+++ InnoClinic.DocumentsMicroservice/Application/Services/DocumentsService.cs
@@ -0,0 +1,54 @@
+﻿using Application.DTOs.Incoming;
+using Application.DTOs.Outgoing;
+using Application.Services.Abstractions;
+using Domain.Abstractions;
+using Domain.Entities;
+using Domain.Models;
+using MimeTypes;
+
+namespace Application.Services;
+
+public class DocumentsService : IDocumentsService
+{
+    private readonly IBlobRepository _blobRepository;
+    private readonly IDocumentsRepository _documentsRepository;
+
+    public DocumentsService(IBlobRepository blobRepository, IDocumentsRepository documentsRepository)
+    {
+        _blobRepository = blobRepository;
+        _documentsRepository = documentsRepository;
+    }
+
+    public async Task<string> UploadAsync(DocumentIncomingDto incomingDto, string partitionName)
+    {
+        var document = new Document();
+        document.PartitionKey = partitionName;
+        document.RowKey = Guid.NewGuid().ToString();
+        var fileExtension = incomingDto.Name.Split('.').Last();
+        document.FileName = document.RowKey + '.' + fileExtension;
+        var stream = new MemoryStream(incomingDto.Base64Value);
+
+        var contentType = MimeTypeMap.GetMimeType(fileExtension);
+
+        var uri = await _blobRepository.UploadAsync(stream, document.FileName, contentType);
+        await _documentsRepository.CreateAsync(document);
+        return document.FileName;
+    }
+
+    public async Task<DocumentOutgoingDto> DownloadAsync(string documentName, string partitionName)
+    {
+        var documentId = documentName.Split('.').First();
+        var document = await _documentsRepository.GetByIdAsync(documentId, partitionName);
+        var documentInfo = await _blobRepository.DownloadAsync(document.FileName);
+        var outgoingDto = new DocumentOutgoingDto();
+        outgoingDto.Stream = documentInfo.Stream;
+        outgoingDto.ContentType = documentInfo.ContentType;
+        return outgoingDto;
+    }
+
+    public async Task DeleteAsync(string documentName, string partitionName)
+    {
+        var documentId = documentName.Split('.').First();
+        await _documentsRepository.DeleteAsync(documentId, partitionName);
+    }
+}
