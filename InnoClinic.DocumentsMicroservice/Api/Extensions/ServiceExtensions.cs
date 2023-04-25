051dc5887cfa35ef7b8b86d22b38d4a47bb1833f
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-25 13:26:43 +0300

    add dependencies to di

---
+++ InnoClinic.DocumentsMicroservice/Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Application.Services;
+using Application.Services.Abstractions;
+using Domain.Abstractions;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Azure;
+using System;
+using Microsoft.Extensions.Configuration;
+using Azure.Storage.Blobs;
+using Azure.Core.Extensions;
+
+namespace Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureAzureStorage(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAzureClients(clientBuilder =>
+        {
+            clientBuilder.AddBlobServiceClient(configuration.GetValue<string>("ConnectionStrings:AzureBlobStorageConnection") ??
+                                               throw new NotImplementedException());
+            clientBuilder.AddTableServiceClient(configuration.GetValue<string>("ConnectionStrings:AzureBlobStorageConnection") ??
+                                                throw new NotImplementedException());
+        });
+    }
+
+    public static void ConfigureRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IBlobRepository, BlobRepository>();
+        services.AddScoped<IDocumentsRepository, DocumentsRepository>();
+    }
+
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IDocumentsService, DocumentsService>();
+    }
+}
--- InnoClinic.DocumentsMicroservice/Api/Program.cs
+++ InnoClinic.DocumentsMicroservice/Api/Program.cs
@@ -1,7 +1,12 @@
+using Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.ConfigureAzureStorage(builder.Configuration);
+builder.Services.ConfigureRepositories();
+builder.Services.ConfigureServices();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
