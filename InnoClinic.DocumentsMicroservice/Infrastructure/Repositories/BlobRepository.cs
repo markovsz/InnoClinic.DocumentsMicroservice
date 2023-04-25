e0dffda9a48fa3bc69e515f83a3e859ce62e050d
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-25 13:25:51 +0300

    add repositories implementation

---
+++ InnoClinic.DocumentsMicroservice/Infrastructure/Repositories/BlobRepository.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using Domain.Models;
+using Domain.Abstractions;
+
+namespace Infrastructure.Repositories;
+
+public class BlobRepository : IBlobRepository
+{
+    protected readonly string _connectionString;
+    protected readonly string _blobContainerName;
+    string t;
+
+    public BlobRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("AzureBlobStorageConnection");
+        t = configuration.GetSection("AzureBlobStorage").Value;
+        _blobContainerName = configuration.GetSection("AzureBlobStorage").GetSection("BlobContainer").Value;
+    }
+
+    public async Task<string> UploadAsync(Stream fileStream, string fileName, string contentType)
+    {
+        var blob = await GetBlobClientAsync(_blobContainerName, fileName);
+        await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+
+        await blob.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = contentType });
+        return blob.Uri.ToString();
+    }
+
+    public async Task<DocumentInfo> DownloadAsync(string fileName)
+    {
+        var blob = await GetBlobClientAsync(_blobContainerName, fileName);
+        var fileStream = new MemoryStream();
+        var response = await blob.DownloadToAsync(fileStream);
+        var documentInfo = new DocumentInfo();
+        fileStream.Position = 0;
+        documentInfo.Stream = fileStream;
+        documentInfo.ContentType = response.Headers.ContentType;
+        return documentInfo;
+    }
+
+    private async Task<BlobClient> GetBlobClientAsync(string containerName, string fileName)
+    {
+        var container = new BlobContainerClient(_connectionString, containerName);
+        var createResponse = await container.CreateIfNotExistsAsync();
+        if (createResponse != null && createResponse.GetRawResponse().Status == 201)
+            await container.SetAccessPolicyAsync(PublicAccessType.Blob);
+
+        var blobClient = container.GetBlobClient(fileName);
+        return blobClient;
+    }
+}
---
+++ InnoClinic.DocumentsMicroservice/Infrastructure/Repositories/DocumentsRepository.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using Domain.Abstractions;
+using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Repositories;
+
+public class DocumentsRepository : IDocumentsRepository
+{
+    private readonly TableClient _tableClient;
+
+	public DocumentsRepository(TableServiceClient serviceClient, IConfiguration configuration)
+	{
+		var documentsTable = configuration.GetSection("AzureTables:DocumentsTable").Value;
+		_tableClient = serviceClient.GetTableClient(documentsTable);
+	}
+
+    public async Task CreateAsync(Document document)
+    {
+        var response = await _tableClient.UpsertEntityAsync(document);
+        if (response.IsError)
+            throw new InvalidOperationException();
+    }
+
+    public async Task<Document> GetByIdAsync(string id, string type)
+    {
+        var response = await _tableClient.GetEntityAsync<Document>(type, id);
+        var document = response.Value;
+        return document;
+    }
+
+    public async Task DeleteAsync(string id, string type)
+    {
+        var response = await _tableClient.DeleteEntityAsync(type, id);
+        if (response.IsError)
+            throw new InvalidOperationException();
+    }
+}
