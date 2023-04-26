3069827b14a00f46a4b3baadec1a2e59441cfc88
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-26 13:56:26 +0300

    add exceptions handler

--- InnoClinic.DocumentsMicroservice/Api/Program.cs
+++ InnoClinic.DocumentsMicroservice/Api/Program.cs
@@ -1,4 +1,5 @@
 using Api.Extensions;
+using Api.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionsHandler>();
 
 app.UseHttpsRedirection();
 
